7b3071cabb722bd1a3e6a3100a65698ce8dd25ac
Benjamin Dehli <superelg@gmail.com>
2022-09-29 10:58:04 +0200

    Hide boilerplate stories

--- src/.oldStories/Button.stories.ts
+++ src/.oldStories/Button.stories.ts
@@ -18,6 +18,7 @@ export default {
 // More on component templates: https://storybook.js.org/docs/web-components/writing-stories/introduction#using-args
 const Template: Story<Partial<ButtonProps>> = (args) => Button(args);
 
+
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/web-components/writing-stories/args
 Primary.args = {
