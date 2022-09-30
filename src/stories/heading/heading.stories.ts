c7e2b313775c42be23ea8c6c1723c9cf44272ff8
Ole <ole@arkitektum.no>
2022-09-30 11:23:47 +0200

    wip headings

---
+++ src/stories/heading/heading.stories.ts
@@ -0,0 +1,28 @@
+import { Meta } from '@storybook/web-components';
+import { Story, Canvas } from '@storybook/addon-docs/blocks';
+import { html } from 'lit-html';
+import { HeadingText } from './heading';
+
+// More on default export: https://storybook.js.org/docs/web-components/writing-stories/introduction#default-export
+export default {
+  title: 'Example/Heading',
+  // More on argTypes: https://storybook.js.org/docs/web-components/api/argtypes
+  argTypes: {
+    size: {h1: 'h1',h2: 'h2',h3: 'h3',h4: 'h4'}
+  },
+} as Meta;
+
+// More on component templates: https://storybook.js.org/docs/web-components/writing-stories/introduction#using-args
+
+const htmlContent = document.createElement('h1');
+
+htmlContent.innerHTML = 'Overskrift';
+
+const Template = (props) => html`<heading>${props.children}</heading>`;
+
+
+export const Default = Template.bind({});
+// More on args: https://storybook.js.org/docs/web-components/writing-stories/args
+Default.args = {
+  children: html`<h1>normal tekst</h1>`
+};
